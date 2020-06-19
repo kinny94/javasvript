e74b229c22fd6b9678f3494c48be56fb3fc3531d
kinny <ad.sam007@gmail.com>
2020-06-19 17:09:31 -0400

    coin toss

---
+++ Coin Toss/app.js
@@ -0,0 +1,27 @@
+const coinArray = ["Heads", "Tails"];
+let score = [0, 0];
+
+const message = document.querySelector(".message");
+const buttons = document.querySelectorAll("button");
+
+for (let i=0; i<buttons.length; i++) {
+    buttons[i].addEventListener('click', tossCoin);
+}
+
+function tossCoin(e) {
+    let playerGuess = e.target.innerText;
+    let computerToss = Math.floor(Math.random() * 2);
+    let computerGuess = coinArray[computerToss];
+    message.innerHTML = "Computer Selected  " + computerGuess + "<br>";
+    let output;
+
+    if (playerGuess === computerGuess) {
+        output = "Player Wins";
+        score[0]++;
+    } else {
+        output = "Computer wins";
+        score[1]++;
+    }
+ 
+    message.innerHTML += output + "<br>Player " + score[0] + "Computer " + score[1];
+}
