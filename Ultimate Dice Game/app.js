2ca0affd53ae3049f4fb7430df9bed8dcbfe9b65
kinny <ad.sam007@gmail.com>
2020-06-23 21:45:21 -0400

    ultimate dice game

--- Ultimate Dice Game/app.js
+++ Ultimate Dice Game/app.js
@@ -5,48 +5,48 @@ const output = document.querySelector("#output");
 const dice = [[5], [1, 9], [1, 5, 9], [1, 3, 7, 9], [1, 3, 5, 7, 9], [1, 3, 4, 6, 7, 9]];   
 
 buttons.addEventListener('click', () => {
-    // let rolls = [roll(6), roll(6)];
-    // let temp;   
-    // if (rolls[0] == rolls[1]) {
-    //     temp = "It was a draw!";
-    // } else if (rolls[0] > rolls[1]) {
-    //     temp = "Player 1 wins!";
-    // } else {
-    //     temp = "Player 2 wins";
-    // }
+    let rolls = [roll(6), roll(6)];
+    let temp;
 
-    let rollDice = roll(6);
-    let holder = builder(rollDice);
-
-    if (output.children[0]) {
-        output.children[0].remove();
+    if (rolls[0] == rolls[1]) {
+        temp = "Draw";
+    } else if (rolls[0] > rolls[1]) {
+        temp = "Player 1 Wins!";
+    } else {
+        temp = "Player 2 Wins!";
     }
-    output.children[0].remove();
-    output.appendChild(holder);
 
-   //output.innerHTML = temp;
-   // player1.innerHTML= rolls[0];
-    //player2.innerHTML = rolls[1];
+    updateOutput(player1, rolls[0]);
+    updateOutput(player2, rolls[1]);
+    output.innerHTML = temp;
 });
 
+function updateOutput(el, num) {
+    let holder = builder(num);
+    if (el.children[0]) {
+        el.children[0].remove();
+    }
+    el.appendChild(holder);
+}
 
-function builder(num) {
+
+function builder(num){
     let div = document.createElement("div");
     let dieArray = dice[num-1];
-    for (let x=1; x<10; x++) {
+    console.log(dieArray);
+    for(let x=1;x<10;x++){
         let span = document.createElement("div");
-        span.setAttribute("class", "dot");
-        if (dieArray.includes(x)) {
-            span.classList.add("black");
+        span.setAttribute("class","dot");
+        if(dieArray.includes(x)){
+            span.classList.add("black");    
         }
         div.appendChild(span);
     }
-    div.setAttribute("class", "dicer");
+    div.setAttribute("class","dicer");
     return div;
 }
+
 function roll(num) {
     let rNumber = Math.floor(Math.random() * num) + 1;
-    let number = 9855 + rNumber;
-    let char = `&#${number};`;
-    return rNumber + " " + char;
+    return rNumber;
 }
