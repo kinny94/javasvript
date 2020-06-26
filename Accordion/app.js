24e6c4d1f66064f57a2cef49183fad530349e5d2
kinny <ad.sam007@gmail.com>
2020-06-26 15:02:35 -0400

    accordion

---
+++ Accordion/app.js
@@ -0,0 +1,10 @@
+const accordion = document.querySelectorAll('.panel');
+accordion.forEach((ele) => {
+    ele.addEventListener('click', () => {
+        const allActives = document.querySelectorAll('.active');
+        allActives.forEach((ele) => {
+            ele.classList.remove('active');
+        })
+        ele.classList.toggle('active');
+    });
+});
