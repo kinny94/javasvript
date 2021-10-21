2db79588d27634c5fbb8178002ab48b90bacf512
Arjun Dass <ad.sam007@gmail.com>
2021-10-20 22:45:28 -0700

    add new css

--- Node/course/basics/routes/shop.js
+++ Node/course/basics/routes/shop.js
@@ -1,11 +1,13 @@
 const path = require('path');
 const express = require('express');
+
 const rootDir = require('../utils/path');
+const adminData = require('./admin');
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    console.log('In another middleware..');
+    console.log('shop.js', adminData.products);
     res.sendFile(path.join(rootDir, 'views', 'shop.html'));
 });
 
