b48f2827db5b345e42e4f923b7ac4098fd45e8b0
Arjun Dass <ad.sam007@gmail.com>
2021-12-27 14:07:03 -0800

    add controllers and models

--- Node/course/basics/routes/admin.js
+++ Node/course/basics/routes/admin.js
@@ -1,25 +1,12 @@
-const path = require('path');
 const express = require("express");
-const rootDir = require('../utils/path');
+const productController = require('../controllers/products');
 
 const router = express.Router();
 
-const products = [];
 
-router.get('/add-product', (req, res, next) => {
-    res.render('add-product', {
-        pageTitle: 'Add Product', 
-        path: '/admin/add-product',
-        formCSS: true,
-        productCSS: true,
-        activeAddProduct: true
-    });
-});
 
-router.post('/add-product', (req, res, next) => {
-    products.push({ title: req.body.title })
-    res.redirect('/');
-}); 
+router.get('/add-product', productController.getAddProduct);
 
-exports.routes = router;
-exports.products = products;
+router.post('/add-product', productController.postAddProduct); 
+
+module.exports = router;
--- Node/course/basics/server.js
+++ Node/course/basics/server.js
@@ -2,6 +2,7 @@ const http = require('http');
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+const errorController = require('./controllers/error');
 
 const app = express();
 
@@ -9,18 +10,16 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-const adminData = require('./routes/admin');
+const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminData.routes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-    res.status(404).render('404', {pageTitle: 'Page not found!'});
-});
+app.use(errorController.get404);
 
 app.listen(3000);  
 
