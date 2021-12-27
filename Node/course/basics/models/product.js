f0dec386d84e0f284a911b2bfbaeec0259247764
Arjun Dass <ad.sam007@gmail.com>
2021-12-27 14:29:15 -0800

    reading and saving to a file

--- Node/course/basics/models/product.js
+++ Node/course/basics/models/product.js
@@ -1,4 +1,17 @@
-const products = [];
+const fs = require('fs');
+const path = require('path');
+
+const p = path.join(path.dirname(process.mainModule.filename), 'data', 'products.json');
+
+const getProductsFromFile = (callback) => {
+    fs.readFile(p, (err, fileContent) => {
+        if (err) {
+            callback([]);
+        } else {
+            callback(JSON.parse(fileContent));
+        }
+    });
+}
 
 module.exports = class Product {
     constructor(title) {
@@ -6,10 +19,15 @@ module.exports = class Product {
     }
 
     save() {
-        products.push(this);
+        getProductsFromFile(products => {
+            products.push(this);
+            fs.writeFile(p, JSON.stringify(products), err => {
+                console.log(err);
+            });
+        });
     }
 
-    static fetchAll() {
-        return products;
+    static fetchAll(callback) {
+        getProductsFromFile(callback);
     }
 }
