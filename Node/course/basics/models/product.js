74fe7e7a92106b891511bc3df9fc547355889935
Arjun Dass <ad.sam007@gmail.com>
2021-12-29 11:45:41 -0800

    adding product detail apge

--- Node/course/basics/models/product.js
+++ Node/course/basics/models/product.js
@@ -23,6 +23,7 @@ module.exports = class Product {
     }
 
     save() {
+        this.id = Math.random().toString();
         getProductsFromFile(products => {
             products.push(this);
             fs.writeFile(p, JSON.stringify(products), err => {
@@ -33,5 +34,12 @@ module.exports = class Product {
 
     static fetchAll(callback) {
         getProductsFromFile(callback);
+    }
+
+    static findById(id, callback) {
+        getProductsFromFile(products => {
+            const product = products.find(p => p.id === id);
+            callback(product);
+        });
     }
 }
